65465223677136bd0a41fc982d8c005ff8a6509c
ysrfeng <ysr200808@163.com>
2017-07-24 14:51:32 +0800

    回调

--- app/src/main/java/com/ysr/qrcore_library/MainActivity.java
+++ app/src/main/java/com/ysr/qrcore_library/MainActivity.java
@@ -1,5 +1,6 @@
 package com.ysr.qrcore_library;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
@@ -10,6 +11,7 @@ import com.ysr.core.util.QRScannerHelper;
 
 public class MainActivity extends AppCompatActivity {
     private QRScannerHelper mScannerHelper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
     /**
      * 在onCreate中调用
      */
@@ -30,11 +33,19 @@ public class MainActivity extends AppCompatActivity {
         mScannerHelper.setCallBack(new QRScannerHelper.OnScannerCallBack() {
             @Override
             public void onScannerBack(String result) {
-                if (!TextUtils.isEmpty(result)){
+                if (!TextUtils.isEmpty(result)) {
                     Toast.makeText(MainActivity.this, result, Toast.LENGTH_SHORT).show();
                 }
 
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (mScannerHelper != null) {
+            mScannerHelper.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
